4e28d3c2da4a524fc3895ce20821404d8c36c392
Anxo Rodriguez <anxolin@gmail.com>
2021-09-16 00:53:12 +0200

    Add websocket

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+  res.sendFile(path.join(__dirname, '/web/index.html'))
+})
+
+app.use(express.static('web'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
 
 server.listen(3000, () => {
-  console.log('listening on *:3000')
+  console.log('🐮🛹 Listening on *:3000')
 })
